590225a9727fd66b4b5f234c4ebb9baaa7ae60ea
Annalia <165366897+annalia12@users.noreply.github.com>
2024-07-18 08:53:24 +0200

    update js second time

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-function displayPoem(response){
-console.log("poem generated");
-new Typewriter("#poem", {
+function displayPoem(response) {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-  });    
+  });
 }
 
-function generatePoem (event) {
-event.preventDefault();
-}
-let instructionsInput = document.querySelector("#user-instructions");
-let apiKey="eat3bf2c0ed301443o16635a4ff05de2";
-let prompt=`User instructions:Generate an Italian poem about ${instructionsInput.value}`;
-let context="You are a romantic Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>.Make sure to follow the user instructions.Do not include a title in the poem.Sign the poem with `SheCodes AI`insise a <strong> element at the end of the poem and NOT at the beginning.";
-let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generatePoem(event) {
+  event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate an Italian poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-
-let poemElement = document.querySelector("#poem");
+  let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a Italian poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an Italian poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
+
